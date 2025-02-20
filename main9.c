e530f5ba0f942d842bab85e73c0661fe2eeaf5d1
darkmage <165251+mikedesu@users.noreply.github.com>
2025-02-20 03:45:35 -0600

    update

---
+++ main9.c
@@ -0,0 +1,163 @@
+#include "raylib.h"
+#include <math.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define MAP_WIDTH 8
+#define MAP_HEIGHT 8
+#define FOV 60.0f
+#define CELL_SIZE 1.0f
+#define PLAYER_OFFSET 0.5f
+
+char map[MAP_HEIGHT][MAP_WIDTH] = {{'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'},
+                                   {'w', '0', '0', '0', '0', '0', '0', 'w'},
+                                   {'w', '0', 'w', '0', 'w', '0', '0', 'w'},
+                                   {'w', '0', '0', '0', '0', 'w', '0', 'w'},
+                                   {'w', '0', 'w', '0', 'w', '0', '0', 'w'},
+                                   {'w', '0', '0', 'w', '0', '0', '0', 'w'},
+                                   {'w', '0', '0', '0', '0', '0', '0', 'w'},
+                                   {'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'}};
+
+typedef struct {
+    Vector2 pos; // Integer grid position (0, 1, 2, etc.)
+    float angle; // Only 0, 90, 180, 270
+    float speed; // Unused, kept for potential future use
+} Player;
+
+bool IsPointInMap(float x, float y) {
+    return x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT;
+}
+
+void GetForwardDirection(float angle, float* dirX, float* dirY) {
+    // Calculate forward direction based on angle
+    if (angle == 0.0f) { // East
+        *dirX = 1.0f;
+        *dirY = 0.0f;
+    } else if (angle == 90.0f) { // North
+        *dirX = 0.0f;
+        *dirY = -1.0f;
+    } else if (angle == 180.0f) { // West
+        *dirX = -1.0f;
+        *dirY = 0.0f;
+    } else if (angle == 270.0f) { // South
+        *dirX = 0.0f;
+        *dirY = 1.0f;
+    }
+}
+
+int main(void) {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Raycasting FPS");
+    SetTargetFPS(60);
+
+    Player player = {
+        .pos = {1.0f, 1.0f}, // Start at grid position (1, 1)
+        .angle = 0.0f, // Start facing east
+        .speed = 5.0f // Unused
+    };
+
+    bool showDebugMap = true;
+
+    while (!WindowShouldClose()) {
+        float deltaTime = GetFrameTime();
+
+        if (IsKeyPressed(KEY_M)) showDebugMap = !showDebugMap;
+
+        // Grid-based movement (1.0 unit steps)
+        float dirX, dirY;
+        if (IsKeyPressed(KEY_W)) { // Move forward
+            GetForwardDirection(player.angle, &dirX, &dirY);
+            float newX = player.pos.x + dirX * CELL_SIZE;
+            float newY = player.pos.y + dirY * CELL_SIZE;
+            if (IsPointInMap(newX, newY) && map[(int)newY][(int)newX] != 'w') {
+                player.pos.x = newX;
+                player.pos.y = newY;
+            }
+        }
+        if (IsKeyPressed(KEY_S)) { // Move backward
+            GetForwardDirection(player.angle, &dirX, &dirY);
+            float newX = player.pos.x - dirX * CELL_SIZE; // Opposite direction
+            float newY = player.pos.y - dirY * CELL_SIZE;
+            if (IsPointInMap(newX, newY) && map[(int)newY][(int)newX] != 'w') {
+                player.pos.x = newX;
+                player.pos.y = newY;
+            }
+        }
+
+        // Discrete 90-degree rotation
+        if (IsKeyPressed(KEY_A)) { // Turn left
+            player.angle += 90.0f;
+            if (player.angle >= 360.0f) player.angle -= 360.0f;
+        }
+        if (IsKeyPressed(KEY_D)) { // Turn right
+            player.angle -= 90.0f;
+            if (player.angle < 0.0f) player.angle += 360.0f;
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        // Raycasting with offset
+        int numRays = showDebugMap ? SCREEN_WIDTH / 2 : SCREEN_WIDTH;
+        float rayAngleStep = FOV / (float)numRays;
+
+        for (int i = 0; i < numRays; i++) {
+            float rayAngle = player.angle - (FOV / 2.0f) + (i * rayAngleStep);
+            float rayX = player.pos.x + PLAYER_OFFSET;
+            float rayY = player.pos.y + PLAYER_OFFSET;
+            float rayCos = cosf(rayAngle * DEG2RAD) / 32.0f;
+            float raySin = sinf(rayAngle * DEG2RAD) / 32.0f;
+            float distance = 0;
+            bool hitWall = false;
+
+            while (distance < 20) {
+                rayX += rayCos;
+                rayY += raySin;
+                distance = sqrtf(powf(rayX - (player.pos.x + PLAYER_OFFSET), 2) +
+                                 powf(rayY - (player.pos.y + PLAYER_OFFSET), 2));
+
+                if (!IsPointInMap(rayX, rayY)) {
+                    int columnX = showDebugMap ? SCREEN_WIDTH / 2 + i * 2 : i;
+                    DrawRectangle(columnX, 0, showDebugMap ? 2 : 1, SCREEN_HEIGHT, BLACK);
+                    hitWall = true;
+                    break;
+                }
+
+                if (map[(int)rayY][(int)rayX] == 'w') {
+                    float wallHeight = (SCREEN_HEIGHT / distance) * 2;
+                    int columnX = showDebugMap ? SCREEN_WIDTH / 2 + i * 2 : i;
+                    DrawRectangle(columnX, SCREEN_HEIGHT / 2 - wallHeight / 2, showDebugMap ? 2 : 1, wallHeight, BLUE);
+                    hitWall = true;
+                    break;
+                }
+            }
+
+            if (!hitWall && IsPointInMap(rayX, rayY)) {
+                int columnX = showDebugMap ? SCREEN_WIDTH / 2 + i * 2 : i;
+                DrawRectangle(columnX, 0, showDebugMap ? 2 : 1, SCREEN_HEIGHT, DARKGRAY);
+            }
+        }
+
+        if (showDebugMap) {
+            for (int y = 0; y < MAP_HEIGHT; y++) {
+                for (int x = 0; x < MAP_WIDTH; x++) {
+                    if (map[y][x] == 'w') {
+                        DrawRectangle(x * 32, y * 32, 32, 32, GRAY);
+                    }
+                }
+            }
+            DrawCircle((player.pos.x + PLAYER_OFFSET) * 32, (player.pos.y + PLAYER_OFFSET) * 32, 5, RED);
+            DrawLine((player.pos.x + PLAYER_OFFSET) * 32,
+                     (player.pos.y + PLAYER_OFFSET) * 32,
+                     (player.pos.x + PLAYER_OFFSET) * 32 + cosf(player.angle * DEG2RAD) * 20,
+                     (player.pos.y + PLAYER_OFFSET) * 32 + sinf(player.angle * DEG2RAD) * 20,
+                     RED);
+        }
+
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
